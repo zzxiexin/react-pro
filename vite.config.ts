064e151f954563f081da5217d8326e198cfa2db6
bujianjia <bujianjia@pinduoduo.com>
2023-12-21 15:35:21 +0800

    feat: 新增别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path, { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@pages': resolve(__dirname, 'src/pages'),
+      '@store': resolve(__dirname, 'src/store'),
+      '@assets': resolve(__dirname, 'src/assets'),
+    },
+  },
+});
