8f3c3562365626c05e191edb52ecf7435f2dc68f
zzxiexin <344391941.com>
2024-11-01 22:33:18 +0800

    feat: 版本限制

--- src/config/router/Auth/requireAuth.tsx
+++ src/config/router/Auth/requireAuth.tsx
@@ -3,6 +3,7 @@ import userInfo from '@store/index';
 import { useAtom } from 'jotai';
 const RequireAuth = ({ children }: { children: JSX.Element }) => {
   const [auth] = useAtom(userInfo);
+  console.log(auth)
   const location = useLocation();
 
   if (!auth.name) {
